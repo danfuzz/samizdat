682a7bf25e4411ab88adfa487ab1893d404fa9de
Dan Bornstein <danfuzz@milk.com>
2013-04-02 13:39:38 -0700

    Settle on a copyright header.

--- c-api/sam-exec.h
+++ c-api/sam-exec.h
@@ -1,5 +1,7 @@
 /*
- * Samizdat. Copyright Dan Bornstein.
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
  */
 
 /*
