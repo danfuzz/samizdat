7f0f86b75d04e0669ddb4dd38230b6242110f143
Dan Bornstein <danfuzz@milk.com>
2013-04-03 16:05:14 -0700

    A bit more of maplet.

--- sam-data-0/maplet.c
+++ sam-data-0/maplet.c
@@ -4,13 +4,31 @@
  * Version 2.0. See the associated file "LICENSE.md" for details.
  */
 
+#include "alloc.h"
 #include "impl.h"
 #include "util.h"
 
+#include <stdlib.h>
+
+
+/*
+ * Helper definitions
+ */
+
+/** The empty maplet, lazily initialized. */
+static zvalue theEmptyMaplet = NULL;
+
+
+/*
+ * API Implementation
+ */
+
 /** Documented in API header. */
 zmapping samMapletGet(zvalue maplet, zint n) {
-    // TODO: Stuff goes here.
-    samDie("TODO");
+    samAssertMaplet(maplet);
+    samAssertNth(maplet, n);
+
+    return ((SamMaplet *) maplet)->elems[n];
 }
 
 /** Documented in API header. */
@@ -21,8 +39,11 @@ zint samMapletFind(zvalue maplet, zvalue key) {
 
 /** Documented in API header. */
 zvalue samMapletEmpty(void) {
-    // TODO: Stuff goes here.
-    samDie("TODO");
+    if (theEmptyMaplet == NULL) {
+	theEmptyMaplet = samAllocValue(SAM_MAPLET, 0, 0);
+    }
+
+    return theEmptyMaplet;
 }
 
 /** Documented in API header. */
