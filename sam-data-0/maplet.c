ef0220e4bbdda1ed2060f8642553784d79538252
Dan Bornstein <danfuzz@milk.com>
2013-04-04 14:34:25 -0700

    Finish up the maplet implementation.

--- sam-data-0/maplet.c
+++ sam-data-0/maplet.c
@@ -9,6 +9,7 @@
 #include "util.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 
 /*
@@ -33,8 +34,31 @@ zmapping samMapletGet(zvalue maplet, zint n) {
 
 /** Documented in API header. */
 zint samMapletFind(zvalue maplet, zvalue key) {
-    // TODO: Stuff goes here.
-    samDie("TODO");
+    samAssertValid(key);
+    samAssertMaplet(maplet);
+
+    zmapping *elems = ((SamMaplet *) maplet)->elems;
+    zint min = 0;
+    zint max = maplet->size - 1;
+
+    while (min <= max) {
+        zint guess = (min + max) / 2;
+        zcomparison c = samCompare(key, elems[guess].key);
+        if (c == SAM_IS_LESS) {
+            max = guess - 1;
+        } else if (c == SAM_IS_MORE) {
+            min = guess + 1;
+        } else {
+            return guess;
+        }
+    }
+
+    // Not found. The insert point is at `min`. Per the API,
+    // this is represented as `~min` (and not, in particular, as `-max`)
+    // so that an insertion point of `0` can be unambiguously
+    // represented.
+
+    return ~min;
 }
 
 /** Documented in API header. */
@@ -48,6 +72,30 @@ zvalue samMapletEmpty(void) {
 
 /** Documented in API header. */
 zvalue samMapletPut(zvalue maplet, zvalue key, zvalue value) {
-    // TODO: Stuff goes here.
-    samDie("TODO");
+    zint index = samMapletFind(maplet, key);
+    zint size = samSize(maplet);
+    zvalue result;
+
+    if (index >= 0) {
+        // The key exists in the given maplet, so we need to perform
+        // a replacement.
+        result = samAllocValue(SAM_MAPLET, size, sizeof(zmapping));
+        memcpy(((SamMaplet *) result)->elems,
+               ((SamMaplet *) maplet)->elems,
+               size * sizeof(zmapping));
+    } else {
+        // The key wasn't found, so we need to insert a new one.
+        index = ~index;
+        result = samAllocValue(SAM_MAPLET, size + 1, sizeof(zmapping));
+        memcpy(((SamMaplet *) result)->elems,
+               ((SamMaplet *) maplet)->elems,
+               index * sizeof(zmapping));
+        memcpy(((SamMaplet *) result)->elems + index + 1,
+               ((SamMaplet *) maplet)->elems + index,
+               (size - index) * sizeof(zmapping));
+    }
+
+    ((SamMaplet *) result)->elems[index].key = key;        
+    ((SamMaplet *) result)->elems[index].value = value;
+    return result;
 }
