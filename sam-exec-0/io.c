f1574e517b652a69525810a678e8c46954c873ee
Dan Bornstein <danfuzz@milk.com>
2013-04-06 14:11:30 -0700

    Flesh out file reading.

--- sam-exec-0/io.c
+++ sam-exec-0/io.c
@@ -6,12 +6,45 @@
 
 #include "sam-exec.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+enum {
+    /** Maximum readable file size, in bytes. */
+    MAX_FILE_SIZE = 100000
+};
+
+
 /*
  * API implementation
  */
 
 /** Documented in API header. */
 zvalue samReadFile(zvalue fileName) {
-    // TODO
-    return 0;
+    zint nameSize = samStringletUtf8Size(fileName);
+    zbyte nameUtf[nameSize + 1];
+
+    samStringletEncodeUtf8(fileName, nameUtf);
+    nameUtf[nameSize] = '\0';
+
+    FILE *in = fopen((char *) nameUtf, "r");
+    if (in == NULL) {
+        samDie("Trouble opening file \"%s\": %s", nameUtf, strerror(errno));
+    }
+
+    zbyte buf[MAX_FILE_SIZE];
+    size_t amt = fread(buf, 1, sizeof(buf), in);
+
+    if (ferror(in)) {
+        samDie("Trouble reading file \"%s\": %s", nameUtf, strerror(errno));
+    }
+
+    if (!feof(in)) {
+        samDie("Overlong file \"%s\": %s", nameUtf, strerror(errno));
+    }
+
+    fclose(in);
+
+    return samStringletFromUtf8String(buf, amt);
 }
