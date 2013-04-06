4771182ee0a9bb3ebdf229d6f25469a6ff443b8e
Dan Bornstein <danfuzz@milk.com>
2013-04-05 23:02:03 -0700

    Skeleton.

---
+++ sam-exec-0/README.md
@@ -0,0 +1,4 @@
+sam-exec-0
+==========
+
+Minimal C implementation of the API defined by `c-api/sam-exec.h`.
---
+++ sam-exec-0/io.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+#include "sam-exec.h"
+
+/** Documented in API header. */
+zvalue samReadFile(zvalue fileName) {
+    // TODO
+    return 0;
+}
