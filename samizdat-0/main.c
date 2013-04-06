318914ff24cd8478cea5e9ac48bbec2e99fc6cac
Dan Bornstein <danfuzz@milk.com>
2013-04-06 14:11:52 -0700

    Minimal main for Samizdat Layer 0.

---
+++ samizdat-0/main.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+#include "sam-exec.h"
+
+#include <string.h>
+
+/**
+ * Main driver for Samizdat Layer 0. Reads in each file named as
+ * an argument, parses it, and then executes the result.
+ */
+int main(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        zvalue name =
+            samStringletFromUtf8String((zbyte *) *argv, strlen(*argv));
+        zvalue file = samReadFile(name);
+
+        // TODO: Something real.
+        samNote("File contents:");
+
+        zint size = samStringletUtf8Size(file);
+        zbyte utf[size + 1];
+        samStringletEncodeUtf8(file, utf);
+        utf[size] = '\0';
+        samNote("%s", utf);
+        samNote("[fin]");
+    }
+}
