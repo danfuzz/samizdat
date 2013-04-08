8930be4357bcdc303cfc37784a8098000b791481
Dan Bornstein <danfuzz@milk.com>
2013-04-08 14:13:41 -0700

    New function registry stuff.
    This is the evolution of the "magic" stuff that had been stubbed out
    in the execution code.

---
+++ samizdat-0/function.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+#include "function.h"
+
+#include <stdlib.h>
+
+enum {
+    /** Maximum number of entries in a function registry */
+    MAX_REGISTRY_SIZE = 100000
+};
+
+/**
+ * Representation of a function in a registry.
+ */
+typedef struct {
+    /** The uniqlet identifier. */
+    zvalue id;
+
+    /** The low-level callable function. */
+    zfunction function;
+
+    /** Arbitrary closure state. */
+    void *state;
+}
+Function;
+
+/* Documented in `function.h` */
+typedef struct FunctionRegistry {
+    /** The entries. */
+    Function entries[MAX_REGISTRY_SIZE];
+
+    /** Count of entries added. */
+    zint size;
+}
+FunctionRegistry;
+
+
+/*
+ * Helper functions
+ */
+
+/**
+ * Finds the entry with the given id. Note: This relies on uniqlets
+ * sorting in the order they were created.
+ */
+static Function *findEntry(zfunreg reg, zvalue id) {
+    Function *entries = reg->entries;
+    zint min = 0;
+    zint max = reg->size - 1;
+
+    while (min <= max) {
+        zint guess = (min + max) / 2;
+        switch (samCompare(id, entries[guess].id)) {;
+            case SAM_IS_LESS: max = guess - 1; break;
+            case SAM_IS_MORE: min = guess + 1; break;
+            default:          return &entries[guess];
+        }
+    }
+
+    samDie("No such function.");
+}
+
+/*
+ * Exported functions
+ */
+
+/* Documented in `function.h` */
+zfunreg funNew(void) {
+    // TODO: Make samAlloc() accessible from here, and use it.
+
+    zfunreg reg = malloc(sizeof(FunctionRegistry));
+    reg->size = 0;
+
+    return reg;
+}
+
+/* Documented in `function.h` */
+zvalue funAdd(zfunreg reg, zfunction function, void *state) {
+    if (reg->size == MAX_REGISTRY_SIZE) {
+        samDie("Too many functions.");
+    }
+
+    Function *entry = &reg->entries[reg->size];
+
+    entry->id = samUniqlet();
+    entry->function = function;
+    entry->state = state;
+    reg->size++;
+
+    return entry->id;
+}
+
+/* Documented in `function.h` */
+zvalue funCall(zfunreg reg, zvalue id, zint argCount, const zvalue *args) {
+    Function *entry = findEntry(reg, id);
+
+    return entry->function(entry->state, argCount, args);
+}
---
+++ samizdat-0/function.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+/*
+ * Function registry and lookup
+ */
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "sam-data.h"
+
+/**
+ * Function registry. The contents of a registry are *not* directly
+ * accessible through instances of this type via the API. You
+ * have to use the various accessor functions.
+ */
+typedef struct FunctionRegistry *zfunreg;
+
+/**
+ * Prototype of all bound functions.
+ */
+typedef zvalue (*zfunction)(void *state, zint argCount, const zvalue *args);
+
+/**
+ * Constructs and returns a new function registry.
+ */
+zfunreg funNew(void);
+
+/**
+ * Adds a function with associated (and arbitrary) closure
+ * state. Returns the identifying uniqlet that binds to it.
+ */
+zvalue funAdd(zfunreg reg, zfunction function, void *state);
+
+/**
+ * Calls the function bound to the given uniqlet.
+ */
+zvalue funCall(zfunreg reg, zvalue id, zint argCount, const zvalue *args);
+
+#endif
