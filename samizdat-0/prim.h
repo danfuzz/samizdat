91c5265c8d38cfede8a4297f9860aad5354ee3e9
Dan Bornstein <danfuzz@milk.com>
2013-04-09 14:59:03 -0700

    Stub of a new module for primitive functions.

---
+++ samizdat-0/prim.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+/*
+ * Primitive function library
+ */
+
+#ifndef _PRIM_H_
+#define _PRIM_H_
+
+#include "lang.h"
+
+/**
+ * Constructs and returns a fresh execution context, containing
+ * bindings for all the primitive functions.
+ */
+zcontext primContext(void);
+
+#endif
---
+++ samizdat-0/prim/prim.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. See the associated file "LICENSE.md" for details.
+ */
+
+#include "prim.h"
+#include "util.h"
+
+
+/*
+ * Exported functions
+ */
+
+/* Documented in header. */
+zcontext primContext(void) {
+    die("TODO");
+}
