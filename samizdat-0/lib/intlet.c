fa7cbb372bdc133aee29e49d0a0ba2312eee694f
Dan Bornstein <danfuzz@milk.com>
2013-04-21 12:46:16 -0700

    Reduce boilerplate in intlet definitions.

--- samizdat-0/lib/intlet.c
+++ samizdat-0/lib/intlet.c
@@ -6,44 +6,25 @@
 
 #include "impl.h"
 
+#define UNARY_PRIM(name, op) \
+    PRIM_IMPL(name) { \
+        requireExactly(argCount, 1); \
+        return datIntletFromInt(op datIntFromIntlet(args[0])); \
+    } \
+    extern int semicolonRequiredHere
 
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(ineg) {
-    requireExactly(argCount, 1);
-    return datIntletFromInt(-datIntFromIntlet(args[0]));
-}
+#define BINARY_PRIM(name, op) \
+    PRIM_IMPL(name) { \
+        requireExactly(argCount, 2); \
+        return datIntletFromInt( \
+            datIntFromIntlet(args[0]) op datIntFromIntlet(args[1])); \
+    } \
+    extern int semicolonRequiredHere
 
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(iadd) {
-    requireExactly(argCount, 2);
-    return datIntletFromInt(
-        datIntFromIntlet(args[0]) + datIntFromIntlet(args[1]));
-}
-
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(isub) {
-    requireExactly(argCount, 2);
-    return datIntletFromInt(
-        datIntFromIntlet(args[0]) - datIntFromIntlet(args[1]));
-}
-
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(imul) {
-    requireExactly(argCount, 2);
-    return datIntletFromInt(
-        datIntFromIntlet(args[0]) * datIntFromIntlet(args[1]));
-}
-
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(idiv) {
-    requireExactly(argCount, 2);
-    return datIntletFromInt(
-        datIntFromIntlet(args[0]) / datIntFromIntlet(args[1]));
-}
-
-/* Documented in Samizdat Layer 0 spec. */
-PRIM_IMPL(imod) {
-    requireExactly(argCount, 2);
-    return datIntletFromInt(
-        datIntFromIntlet(args[0]) % datIntFromIntlet(args[1]));
-}
+/* These are all documented in Samizdat Layer 0 spec. */
+UNARY_PRIM(ineg, -);
+BINARY_PRIM(iadd, +);
+BINARY_PRIM(idiv, /);
+BINARY_PRIM(imod, %);
+BINARY_PRIM(imul, *);
+BINARY_PRIM(isub, -);
