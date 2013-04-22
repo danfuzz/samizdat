1cdae75011cafe193e2262344400be0be606bc6d
Dan Bornstein <danfuzz@milk.com>
2013-04-21 19:41:30 -0700

    Pull the contents of `assert.c` into `basic.c`.
    This mirrors the arrangement in the `dat.h` header.

--- samizdat-0/dat/basic.c
+++ samizdat-0/dat/basic.c
@@ -6,6 +6,44 @@
 
 #include "impl.h"
 
+#include <stddef.h>
+
+
+/*
+ * Helper functions
+ */
+
+/**
+ * Gets the name of a given type.
+ */
+static const char *typeName(ztype type) {
+    switch (type) {
+        case DAT_INTLET:    return "intlet";
+        case DAT_STRINGLET: return "stringlet";
+        case DAT_LISTLET:   return "listlet";
+        case DAT_MAPLET:    return "maplet";
+        case DAT_UNIQLET:   return "uniqlet";
+        case DAT_HIGHLET:   return "highlet";
+    }
+
+    return "<unknown-type>";
+}
+
+/**
+ * Asserts a particular type.
+ */
+static void assertType(zvalue value, ztype type) {
+    datAssertValid(value);
+
+    if (value->type == type) {
+        return;
+    }
+
+    die("Expected type %s: (%p)->type == %s",
+        typeName(type), value, typeName(value->type));
+}
+
+
 /*
  * Module functions
  */
@@ -19,6 +57,78 @@ bool datHasNth(zvalue value, zint n) {
 /*
  * Exported functions
  */
+
+/* Documented in header. */
+void datAssertValid(zvalue value) {
+    if (value == NULL) {
+        die("Null value.");
+    }
+
+    zint bits = (zint) (void *) value;
+
+    if ((bits % DAT_VALUE_ALIGNMENT) != 0) {
+        die("Bad alignment for value: %p", value);
+    }
+
+    if (value->magic != DAT_VALUE_MAGIC) {
+        die("Incorrect magic for value: (%p)->magic == %#04x",
+            value, value->magic);
+    }
+
+    switch (value->type) {
+        case DAT_INTLET:
+        case DAT_STRINGLET:
+        case DAT_LISTLET:
+        case DAT_MAPLET:
+        case DAT_UNIQLET:
+        case DAT_HIGHLET: {
+            break;
+        }
+        default: {
+            die("Invalid type for value: (%p)->type == %#04x",
+                value, value->type);
+        }
+    }
+}
+
+/* Documented in header. */
+void datAssertNth(zvalue value, zint n) {
+    datAssertValid(value);
+
+    if (!datHasNth(value, n)) {
+        die("Invalid index: %lld; size %lld", n, value->size);
+    }
+}
+
+/* Documented in header. */
+void datAssertIntlet(zvalue value) {
+    assertType(value, DAT_INTLET);
+}
+
+/* Documented in header. */
+void datAssertStringlet(zvalue value) {
+    assertType(value, DAT_STRINGLET);
+}
+
+/* Documented in header. */
+void datAssertListlet(zvalue value) {
+    assertType(value, DAT_LISTLET);
+}
+
+/* Documented in header. */
+void datAssertMaplet(zvalue value) {
+    assertType(value, DAT_MAPLET);
+}
+
+/* Documented in header. */
+void datAssertUniqlet(zvalue value) {
+    assertType(value, DAT_UNIQLET);
+}
+
+/* Documented in header. */
+void datAssertHighlet(zvalue value) {
+    assertType(value, DAT_HIGHLET);
+}
 
 /* Documented in header. */
 zint datSize(zvalue value) {
