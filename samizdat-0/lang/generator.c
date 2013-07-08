d1dbba62ee870704eb155e41c763e214d48d943c
Dan Bornstein <danfuzz@milk.com>
2013-07-08 14:48:24 -0700

    Partial implementation of `listFromGenerator` in samizdat-0.

---
+++ samizdat-0/lang/generator.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
+/*
+ * Generator glue
+ */
+
+#include "impl.h"
+#include "util.h"
+
+/*
+ * Helper functions
+ */
+
+/**
+ * Does listification of a map. This returns a list of individual mappings.
+ */
+static zvalue listFromMap(zvalue value) {
+    die("TODO");
+}
+
+/**
+ * Does listification of a string. This returns a list of individual
+ * characters.
+ */
+static zvalue listFromString(zvalue value) {
+    die("TODO");
+}
+
+/**
+ * Does generator iteration to get a list.
+ */
+static zvalue listFromGeneratorPerSe(zvalue generator) {
+    die("TODO");
+}
+
+
+/*
+ * Module functions
+ */
+
+/* Documented in header. */
+zvalue listFromGenerator(zvalue value) {
+    switch (datType(value)) {
+        case DAT_LIST: {
+            // Trivial pass-through.
+            return value;
+        }
+        case DAT_MAP: {
+            return listFromMap(value);
+        }
+        case DAT_STRING: {
+            return listFromString(value);
+        }
+        case DAT_UNIQLET: {
+            return listFromGeneratorPerSe(value);
+        }
+        default: {
+            die("Invalid type for listFromGenerator.");
+        }
+    }
+}
