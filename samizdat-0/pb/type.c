1dcaa8d2148e328cc1cb36bc882db78f97b91455
Dan Bornstein <danfuzz@milk.com>
2013-08-08 12:23:10 -0700

    Make `Value` be the top type.

--- samizdat-0/pb/type.c
+++ samizdat-0/pb/type.c
@@ -24,6 +24,9 @@ static zvalue theTypes[PB_MAX_TYPES];
  * Payload struct for type `Type`.
  */
 typedef struct {
+    /** Parent type. Only allowed to be `NULL` for `Value`. */
+    zvalue parent;
+
     /**
      * Name of the type. Arbitrary other than that it must be unique
      * among all types.
@@ -53,13 +56,19 @@ static TypeInfo *typeInfo(zvalue type) {
 /**
  * Initializes a type value.
  */
-static void typeInit(zvalue type, zvalue name, zvalue secret, bool derived) {
+static void typeInit(zvalue type, zvalue parent, zvalue name, zvalue secret,
+        bool derived) {
     if (theNextId == PB_MAX_TYPES) {
         die("Too many types!");
     }
 
+    if ((parent == NULL) && (type != TYPE_Value)) {
+        die("Every type but `Value` needs a parent.");
+    }
+
     TypeInfo *info = typeInfo(type);
 
+    info->parent = parent;
     info->name = name;
     info->secret = secret;
     info->id = theNextId;
@@ -87,7 +96,7 @@ static zvalue allocType(void) {
  */
 static zvalue newType(zvalue name, zvalue secret, bool derived) {
     zvalue result = allocType();
-    typeInit(result, name, secret, derived);
+    typeInit(result, TYPE_Value, name, secret, derived);
     return result;
 }
 
@@ -274,11 +283,11 @@ void pbInitTypeSystem(void) {
     // a hackish convenience. It should probably be a Uniqlet.
     PB_SECRET = allocType();
 
-    typeInit(TYPE_Type,    stringFromUtf8(-1, "Type"),    PB_SECRET, false);
-    typeInit(TYPE_Value,   stringFromUtf8(-1, "Value"),   PB_SECRET, false);
-    typeInit(TYPE_String,  stringFromUtf8(-1, "String"),  PB_SECRET, false);
-    typeInit(TYPE_Generic, stringFromUtf8(-1, "Generic"), PB_SECRET, false);
-    typeInit(PB_SECRET,    stringFromUtf8(-1, "SECRET"),  PB_SECRET, false);
+    typeInit(TYPE_Type,    TYPE_Value, stringFromUtf8(-1, "Type"),    PB_SECRET, false);
+    typeInit(TYPE_Value,   NULL,       stringFromUtf8(-1, "Value"),   PB_SECRET, false);
+    typeInit(TYPE_String,  TYPE_Value, stringFromUtf8(-1, "String"),  PB_SECRET, false);
+    typeInit(TYPE_Generic, TYPE_Value, stringFromUtf8(-1, "Generic"), PB_SECRET, false);
+    typeInit(PB_SECRET,    TYPE_Value, stringFromUtf8(-1, "SECRET"),  PB_SECRET, false);
 }
 
 /* Documented in header. */
