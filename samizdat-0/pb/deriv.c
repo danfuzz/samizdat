fd00b1b46c76710899c6765001eb6feba06121a1
Dan Bornstein <danfuzz@milk.com>
2013-08-12 20:34:26 -0700

    Restructure, to be more like other type definitions.

--- samizdat-0/pb/deriv.c
+++ samizdat-0/pb/deriv.c
@@ -29,49 +29,6 @@ static DerivInfo *derivInfo(zvalue value) {
     return (DerivInfo *) pbPayload(value);
 }
 
-/* Documented in header. */
-METH_IMPL(Deriv, eq) {
-    zvalue v1 = args[0];
-    zvalue v2 = args[1];
-
-    return pbEq(derivInfo(v1)->data, derivInfo(v2)->data) ? v2 : NULL;
-}
-
-/* Documented in header. */
-METH_IMPL(Deriv, gcMark) {
-    zvalue value = args[0];
-    pbMark(derivInfo(value)->data);
-    return NULL;
-}
-
-/* Documented in header. */
-METH_IMPL(Deriv, order) {
-    zvalue v1 = args[0];
-    zvalue v2 = args[1];
-    zvalue data1 = derivInfo(v1)->data;
-    zvalue data2 = derivInfo(v2)->data;
-
-    if (data1 == NULL) {
-        return (data2 == NULL) ? PB_0 : PB_NEG1;
-    } else if (data2 == NULL) {
-        return PB_1;
-    } else {
-        return intFromZint(pbOrder(data1, data2));
-    }
-}
-
-
-/*
- * Module Definitions
- */
-
-/* Documented in header. */
-void derivBind(zvalue type) {
-    gfnBindCore(GFN_eq,     type, Deriv_eq);
-    gfnBindCore(GFN_gcMark, type, Deriv_gcMark);
-    gfnBindCore(GFN_order,  type, Deriv_order);
-}
-
 
 /*
  * Exported Definitions
@@ -114,4 +71,50 @@ zvalue derivFrom(zvalue type, zvalue data, zvalue secret) {
 /* Documented in header. */
 zvalue valueFrom(zvalue type, zvalue data) {
     return derivFrom(type, data, NULL);
+}
+
+
+/*
+ * Type Definition
+ *
+ * **Note:** This isn't the usual form of type definition, since these
+ * methods are bound on many types.
+ */
+
+/* Documented in header. */
+METH_IMPL(Deriv, eq) {
+    zvalue v1 = args[0];
+    zvalue v2 = args[1];
+
+    return pbEq(derivInfo(v1)->data, derivInfo(v2)->data) ? v2 : NULL;
+}
+
+/* Documented in header. */
+METH_IMPL(Deriv, gcMark) {
+    zvalue value = args[0];
+    pbMark(derivInfo(value)->data);
+    return NULL;
+}
+
+/* Documented in header. */
+METH_IMPL(Deriv, order) {
+    zvalue v1 = args[0];
+    zvalue v2 = args[1];
+    zvalue data1 = derivInfo(v1)->data;
+    zvalue data2 = derivInfo(v2)->data;
+
+    if (data1 == NULL) {
+        return (data2 == NULL) ? PB_0 : PB_NEG1;
+    } else if (data2 == NULL) {
+        return PB_1;
+    } else {
+        return intFromZint(pbOrder(data1, data2));
+    }
+}
+
+/* Documented in header. */
+void derivBind(zvalue type) {
+    gfnBindCore(GFN_eq,     type, Deriv_eq);
+    gfnBindCore(GFN_gcMark, type, Deriv_gcMark);
+    gfnBindCore(GFN_order,  type, Deriv_order);
 }
