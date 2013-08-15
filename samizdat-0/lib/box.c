3df1a9a4679431df0fba5c7eea7dc2d3441a1055
Dan Bornstein <danfuzz@milk.com>
2013-08-14 21:48:20 -0700

    Split out box functions.

---
+++ samizdat-0/lib/box.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
+#include "impl.h"
+
+
+/*
+ * Exported Definitions
+ */
+
+/* Documented in Samizdat Layer 0 spec. */
+PRIM_IMPL(makeMutableBox) {
+    zvalue value = (argCount == 1) ? args[0] : NULL;
+    return makeMutableBox(value);
+}
+
+/* Documented in Samizdat Layer 0 spec. */
+PRIM_IMPL(makeYieldBox) {
+    return makeYieldBox();
+}
