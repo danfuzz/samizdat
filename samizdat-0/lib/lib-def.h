c2655cedd3de1f7e95a8ec1f8b79c3d0a893fe2c
Dan Bornstein <danfuzz@milk.com>
2013-08-29 15:47:04 -0700

    Rename file, and note that it's specified.

--- samizdat-0/lib/lib-def.h
+++ samizdat-0/lib/lib-def.h
@@ -17,7 +17,7 @@ LIB_FILE(Bitwise,           sam0);
 LIB_FILE(Boolean,           sam0);
 LIB_FILE(Collection,        sam0);
 LIB_FILE(FilterGenerator,   sam0);
-LIB_FILE(Function,          sam0);
+LIB_FILE(ForwardFunction,   sam0);
 LIB_FILE(Generator,         sam0);
 LIB_FILE(ListWrapGenerator, sam0);
 LIB_FILE(NullBox,           sam0);
