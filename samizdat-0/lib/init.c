748f482d08b2b25d8ae9364c46fec34e3477c0ac
Dan Bornstein <danfuzz@milk.com>
2013-09-16 17:16:34 -0700

    Stop pre-reading the library files.

--- samizdat-0/lib/init.c
+++ samizdat-0/lib/init.c
@@ -70,15 +70,6 @@ static void makePrimitiveContext(void) {
 }
 
 /**
- * Reads a file whose name is the two given components, concatenated with
- * a `/` in the middle.
- */
-static zvalue readFile(zvalue directory, zvalue name) {
-    zvalue path = GFN_CALL(cat, directory, STR_CH_SLASH, name);
-    return ioFlatReadFileUtf8(path);
-}
-
-/**
  * Reads and evaluates the file whose name is the two given components,
  * concatenated with a `/` in the middle. Evaluation is done using the
  * primitive-only global context.
@@ -86,7 +77,8 @@ static zvalue readFile(zvalue directory, zvalue name) {
 static zvalue evalFile(zvalue directory, zvalue name) {
     zstackPointer save = pbFrameStart();
 
-    zvalue programText = readFile(directory, name);
+    zvalue path = GFN_CALL(cat, directory, STR_CH_SLASH, name);
+    zvalue programText = ioFlatReadFileUtf8(path);
     zvalue programTree = langParseProgram0(programText);
     zvalue result = langEval0(PRIMITIVE_CONTEXT, programTree);
 
@@ -95,49 +87,15 @@ static zvalue evalFile(zvalue directory, zvalue name) {
 }
 
 /**
- * Sets up the mapping from names to library file contents.
- * This is what ends up bound to `LIBRARY_FILES` in the main entry
- * of `samizdat-0-lib`.
- */
-static zvalue getLibraryFiles(zvalue libraryDir) {
-    zvalue result = EMPTY_MAP;
-
-    // Read the manifest file, and evaluate it. It evaluates to a map
-    // that binds `BOOTSTRAP_FILES` and `MANIFEST`, and those list all
-    // the ultimate library files.
-    zvalue manifestMap = FUN_CALL(evalFile(libraryDir, STR_manifest_sam0));
-    zvalue allFiles = GFN_CALL(cat,
-        collGet(manifestMap, STR_BOOTSTRAP_FILES),
-        collGet(manifestMap, STR_MANIFEST));
-    zint size = collSize(allFiles);
-
-    for (zint i = 0; i < size; i++) {
-        zvalue oneName = collNth(allFiles, i);
-        zvalue text = readFile(libraryDir, oneName);
-        result = collPut(result, oneName, text);
-    }
-
-    return result;
-}
-
-/**
  * Returns a map with all the core library bindings. This is the
  * return value from running the in-language library `main`.
  */
 static zvalue getLibrary(zvalue libraryDir) {
-    zstackPointer save = pbFrameStart();
-
-    zvalue libraryFiles = getLibraryFiles(libraryDir);
-    zvalue mainText = collGet(libraryFiles, STR_main_sam0);
-    zvalue mainProgram = langParseProgram0(mainText);
-
-    zvalue mainFunction = langEval0(PRIMITIVE_CONTEXT, mainProgram);
-
-    pbFrameReturn(save, mainFunction);
+    zvalue mainFunction = evalFile(libraryDir, STR_main_sam0);
 
     // It is the responsibility of the `main` core library program
     // to return the full set of core library bindings.
-    return FUN_CALL(mainFunction, libraryDir, libraryFiles);
+    return FUN_CALL(mainFunction, libraryDir);
 }
 
 
