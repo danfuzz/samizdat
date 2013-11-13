2efa7924eb0fb60dd041d86dcb9a8813cbbddc91
Dan Bornstein <danfuzz@milk.com>
2013-11-12 16:00:05 -0800

    Test of dynamic library creation.
    The idea is to get this to be linkable into a running samizdat-0,
    and then use the same pattern when producing compiled output.

---
+++ dl-test/main/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2013 the Samizdat Authors (Dan Bornstein et alia).
+ * Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+ * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+
+#include "util.h"
+
+static void run(void) __attribute__((constructor));
+static void run(void) {
+    note("=== yo");
+}
