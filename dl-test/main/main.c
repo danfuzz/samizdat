4026857e9a3e9fe5a6fde9124a32a8e6764eda0e
Dan Bornstein <danfuzz@milk.com>
2013-11-14 15:39:06 -0800

    Make this look a little realer.

--- dl-test/main/main.c
+++ dl-test/main/main.c
@@ -4,10 +4,17 @@
  * Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
  */
 
+#include "type/Map.h"
+#include "type/String.h"
 #include "util.h"
 
-// TODO: Maybe also use `__attribute((weak))`.
-static void run(void) __attribute__((constructor));
-static void run(void) {
-    note("=== yo");
+zvalue eval(zvalue context) {
+    note("=== Hello module!");
+
+    zvalue testStr = stringFromUtf8(-1, "TEST");
+    zmapping mappings[] = {
+        { testStr, testStr }
+    };
+
+    return mapFromArray(1, mappings);
 }
