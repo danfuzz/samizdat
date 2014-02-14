d60b6b8ae9d76e5240ded42617e0c76215d94583
Dan Bornstein <danfuzz@milk.com>
2014-02-14 15:02:36 -0800

    New blur file for `samex`.

---
+++ samex/blur.sh
@@ -0,0 +1,42 @@
+# Copyright 2014 the Samizdat Authors (Dan Bornstein et alia).
+# Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+# Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+
+#
+# Argument parsing
+#
+
+if [[ $1 != '' ]]; then
+    echo "Unknown option: $1" 1>&2
+    exit 1
+fi
+
+
+#
+# Main script
+#
+
+progDir="$(abs-path .)"
+baseDir="$(abs-path ..)"
+projectName="${progDir##*/}"
+OUT="${OUT:-${baseDir}/out}"
+FINAL="${OUT}/final"
+
+binNames=(
+    "bin/samex"
+    "bin/compile-samex-addon"
+    "helper/find-samex"
+)
+
+rule copy \
+    --id=build \
+    --in-dir="${progDir}" \
+    --target-dir="${FINAL}" \
+    --chmod=755 \
+    -- "${binNames[@]}"
+
+rule rm \
+    --id=clean \
+    --in-dir="${FINAL}" \
+    -- "${binNames[@]}"
