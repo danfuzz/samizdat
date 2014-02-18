51b2c36665fbf660dd05f46fd14202b61fc6174c
Dan Bornstein <danfuzz@milk.com>
2014-02-17 19:22:51 -0800

    Use the public(ish) version of Blur.

--- blur/blur-utils.sh
+++ blur/blur-utils.sh
@@ -1,4 +1,4 @@
-# Copyright 2014 the Samizdat Authors (Dan Bornstein et alia).
+# Copyright 2014 the Blur Authors (Dan Bornstein et alia).
 # Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 # Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
