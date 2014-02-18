c2667ffb5722a17fd3bd55232c83a2492f750e7f
Dan Bornstein <danfuzz@milk.com>
2014-02-17 16:37:03 -0800

    First cut of top-level build file.

---
+++ blur.sh
@@ -0,0 +1,28 @@
+# Copyright 2014 the Samizdat Authors (Dan Bornstein et alia).
+# Licensed AS IS and WITHOUT WARRANTY under the Apache License,
+# Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
+
+
+#
+# Argument parsing
+#
+
+if [[ $1 != '' ]]; then
+    echo "Unknown option: $1" 1>&2
+    exit 1
+fi
+
+
+#
+# Main script
+#
+
+OUT="${BASE_DIR}/out"
+
+rule body \
+    --id=build \
+    --build-in-dir="samex-tot"
+
+rule rm \
+    --id=clean \
+    -- "${OUT}"
