af1f40db1719f0fc65add0395b2d52bf955a1406
Dan Bornstein <danfuzz@milk.com>
2014-02-19 14:17:01 -0800

    Add detail.

--- blur/blur-utils.sh
+++ blur/blur-utils.sh
@@ -3,7 +3,7 @@
 # Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
 #
-# Helper library. Meant to be included via `.` command.
+# Helper library. Meant to be included via `.` command or `BASH_ENV` variable.
 #
 
 
