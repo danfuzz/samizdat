b57c88c2f36002e0a27fa219e22df2f269c8a25b
Dan Bornstein <danfuzz@milk.com>
2014-08-01 16:19:03 -0700

    Expose `objectDataOf`.

--- samex-naif/lib/prim-def.h
+++ samex-naif/lib/prim-def.h
@@ -55,6 +55,9 @@ PRIM_DEF(Bitwise_xor,             SEL_NAME(xor));
 // Generic functions: `Box` protocol.
 PRIM_DEF(store,                   SEL_NAME(store));
 
+// Generic functions: `Object` class.
+PRIM_DEF(objectDataOf,            SEL_NAME(objectDataOf));
+
 // Generic functions: `DerivedData` class.
 PRIM_DEF(dataOf,                  SEL_NAME(dataOf));
 
