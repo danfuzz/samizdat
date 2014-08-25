d1b9118add7a795c61bab41db29d8425b8b8baa6
Dan Bornstein <danfuzz@milk.com>
2014-08-25 12:32:57 -0700

    Pass selectors in `makeObjectClass`.

--- samex-naif/dat/Object.c
+++ samex-naif/dat/Object.c
@@ -67,7 +67,7 @@ zvalue makeObject(zvalue cls, zvalue secret, zvalue data) {
 
 // Documented in header.
 zvalue makeObjectClass(zvalue name, zvalue secret) {
-    assertHasClass(name, CLS_String);
+    assertHasClass(name, CLS_Selector);
     return makeClass(name, CLS_Object, secret, NULL, NULL);
 }
 
