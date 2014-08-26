f664fe6569d5374936dab2e27818856a492aa918
Dan Bornstein <danfuzz@milk.com>
2014-08-26 15:49:32 -0700

    New method implementation helpers.
    These are made to stop relying on a global per-symbol method signature.
    What I came up with here isn't the prettiest thing in the world, but
    it doesn't seem *too* terrible to me.

--- samex-naif/include/type/define.h
+++ samex-naif/include/type/define.h
@@ -48,4 +48,60 @@
     static zvalue METH_NAME(cls, name)( \
         zvalue thisFunction, zint argCount, const zvalue *args)
 
+/**
+ * Expands to a comma-separated pair of symbol and builtin function,
+ * for the indicated method. This is for use in calls to
+ * `symbolTableFromArgs`.
+ */
+#define METH_BIND(cls, name) \
+    SYM_NAME(name), \
+    MAKE_##cls##_##name()
+
+
+//
+// Method implementation declarations. Each of these is identical except
+// for the argument requirements (not including a `this` argument). The
+// names are `...args` or `...minArgs_maxArgs` with `rest` used to indicate
+// an unbounded number of arguments.
+//
+
+#define METH_IMPL_MIN_MAX(cls, name, minArgs, maxArgs) \
+    static zvalue METH_NAME(cls, name)( \
+            zvalue thisFunction, zint argCount, const zvalue *args); \
+    static zvalue MAKE_##cls##_##name(void) { \
+        return makeBuiltin(minArgs, maxArgs, METH_NAME(cls, name), 0, \
+            stringFromUtf8(-1, #cls "." #name)); \
+    } \
+    static zvalue METH_NAME(cls, name)( \
+            zvalue thisFunction, zint argCount, const zvalue *args)
+
+#define METH_IMPL_0(cls, name) \
+    static zvalue IMPL_##cls##_##name(zvalue); \
+    METH_IMPL_MIN_MAX(cls, name, 1, 1) { \
+        return IMPL_##cls##_##name(args[0]); \
+    } \
+    static zvalue IMPL_##cls##_##name(zvalue ths)
+
+#define METH_IMPL_1(cls, name, a0) \
+    static zvalue IMPL_##cls##_##name(zvalue, zvalue); \
+    METH_IMPL_MIN_MAX(cls, name, 2, 2) { \
+        return IMPL_##cls##_##name(args[0], args[1]); \
+    } \
+    static zvalue IMPL_##cls##_##name(zvalue ths, zvalue a0)
+
+#define METH_IMPL_2(cls, name, a0, a1) \
+    static zvalue IMPL_##cls##_##name(zvalue, zvalue, zvalue); \
+    METH_IMPL_MIN_MAX(cls, name, 3, 3) { \
+        return IMPL_##cls##_##name(args[0], args[1], args[2]); \
+    } \
+    static zvalue IMPL_##cls##_##name(zvalue ths, zvalue a0, zvalue a1)
+
+#define METH_IMPL_rest(cls, name, aRest) \
+    static zvalue IMPL_##cls##_##name(zvalue, zint, zvalue); \
+    METH_IMPL_MIN_MAX(cls, name, 0, -1) { \
+        return IMPL_##cls##_##name(args[0], argCount - 1, &args[1]); \
+    } \
+    static zvalue IMPL_##cls##_##name(zvalue ths, \
+            zint aRest##Size, zvalue *aRest)
+
 #endif
