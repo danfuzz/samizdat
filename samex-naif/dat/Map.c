d0a0904327e50fea9ff327a9a2c29737eb9ffdd1
Dan Bornstein <danfuzz@milk.com>
2014-10-13 15:45:14 -0700

    Rename, for parallelism.

--- samex-naif/dat/Map.c
+++ samex-naif/dat/Map.c
@@ -64,7 +64,7 @@ static zvalue mapFromArrayUnchecked(zint size, zmapping *mappings) {
 /**
  * Constructs and returns a single-mapping map.
  */
-static zvalue makeMapping(zvalue key, zvalue value) {
+static zvalue mapFrom1(zvalue key, zvalue value) {
     zmapping elem = { key, value };
     return mapFromArrayUnchecked(1, &elem);
 }
@@ -85,7 +85,7 @@ static zvalue mapFrom2(zvalue k1, zvalue v1, zvalue k2, zvalue v2) {
             return mapFromArrayUnchecked(2, elems);
         }
         default: {
-            return makeMapping(k2, v2);
+            return mapFrom1(k2, v2);
         }
     }
 }
@@ -182,7 +182,7 @@ zvalue mapFromArray(zint size, zmapping *mappings) {
             return EMPTY_MAP;
         }
         case 1: {
-            return makeMapping(mappings[0].key, mappings[0].value);
+            return mapFrom1(mappings[0].key, mappings[0].value);
         }
         case 2: {
             return mapFrom2(
@@ -469,7 +469,7 @@ METH_IMPL_1(Map, nextValue, box) {
             // Make a mapping for the first element, yield it, and return
             // a map of the remainder.
             zmapping *elems = info->elems;
-            zvalue mapping = makeMapping(elems[0].key, elems[0].value);
+            zvalue mapping = mapFrom1(elems[0].key, elems[0].value);
             cm_store(box, mapping);
             return mapFromArrayUnchecked(size - 1, &elems[1]);
         }
@@ -490,7 +490,7 @@ METH_IMPL_1(Map, nthMapping, n) {
     }
 
     zmapping *m = &info->elems[index];
-    return makeMapping(m->key, m->value);
+    return mapFrom1(m->key, m->value);
 }
 
 // Documented in spec.
@@ -507,7 +507,7 @@ METH_IMPL_2(Map, put, key, value) {
     switch (size) {
         case 0: {
             // `put({}, ...)`
-            return makeMapping(key, value);
+            return mapFrom1(key, value);
         }
         case 1: {
             return mapFrom2(elems[0].key, elems[0].value, key, value);
