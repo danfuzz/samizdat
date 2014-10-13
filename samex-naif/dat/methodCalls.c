b460ed2a92afe60fc338a58b1512d2ab265179e9
Dan Bornstein <danfuzz@milk.com>
2014-10-13 14:59:59 -0700

    Define `cm_new_SymbolTable()`.

--- samex-naif/dat/methodCalls.c
+++ samex-naif/dat/methodCalls.c
@@ -2,9 +2,12 @@
 // Licensed AS IS and WITHOUT WARRANTY under the Apache License,
 // Version 2.0. Details: <http://www.apache.org/licenses/LICENSE-2.0>
 
+#include <stdarg.h>
+
 #include "type/methodCalls.h"
 #include "type/Int.h"
 #include "type/String.h"
+#include "type/SymbolTable.h"
 
 #include "impl.h"
 
@@ -46,6 +49,43 @@ char *cm_debugString(zvalue x) {
 // Documented in header.
 zvalue cm_nth(zvalue x, zint index) {
     return METH_CALL(nth, x, intFromZint(index));
+}
+
+// Documented in header.
+zvalue cm_new_SymbolTable(zvalue first, ...) {
+    if (first == NULL) {
+        return EMPTY_SYMBOL_TABLE;
+    }
+
+    zint argCount = 1;
+    va_list rest;
+
+    va_start(rest, first);
+    for (/*argCount*/; va_arg(rest, zvalue) != NULL; argCount++) /* empty */;
+    va_end(rest);
+
+    if ((argCount & 1) != 0) {
+        die("Odd argument count for symbol table construction.");
+    }
+
+    zint size = argCount >> 1;
+    zmapping elems[size];
+
+    elems[0].key = first;
+
+    va_start(rest, first);
+    for (zint i = 1; i < argCount; i++) {
+        zvalue one = va_arg(rest, zvalue);
+        zmapping *elem = &elems[i >> 1];
+        if ((i & 1) == 0) {
+            elem->key = one;
+        } else {
+            elem->value = one;
+        }
+    }
+    va_end(rest);
+
+    return symbolTableFromArray(size, elems);
 }
 
 // Documented in header.
