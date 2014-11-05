251b19d03cbb9fc1ddbc072efeca10bc63815d50
Dan Bornstein <danfuzz@milk.com>
2014-11-05 15:24:14 -0800

    Remove outdated comment.

--- samex-naif/include/type/Map.h
+++ samex-naif/include/type/Map.h
@@ -22,8 +22,6 @@ extern zvalue EMPTY_MAP;
  * Gets the map resulting from adding all the given mappings
  * to an empty map, in the order given (so, in particular, higher-index
  * mappings take precedence over the lower-index mappings, when keys match).
- * The effect is identical to calling a chain of `put()`s on each
- * of the mappings in order, with the empty map as a base.
  *
  * **Warning:** This function *may* modify the memory pointed at by
  * `mappings`. However, once this function returns, it is safe to reuse
