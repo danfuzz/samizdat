6b186e9d66daab143a2e208de2db6976dace42e4
Dan Bornstein <danfuzz@milk.com>
2014-11-19 11:03:25 -0800

    Implement node conversion.

--- samex-naif/lang/ExecNode.c
+++ samex-naif/lang/ExecNode.c
@@ -10,6 +10,7 @@
 
 #include "const.h"
 #include "type/define.h"
+#include "type/List.h"
 
 #include "impl.h"
 #include "langnode.h"
@@ -20,7 +21,7 @@
 //
 
 /**
- * Payload data. This is a approximately a union (in the math sense, not the C
+ * Payload data. This is approximately a union (in the math sense, not the C
  * sense) of all the possible named bindings of any executable node type.
  * Bound values which are themselves executable nodes are recursively
  * translated. Other values are in some cases left alone and in others
@@ -28,7 +29,7 @@
  */
 typedef struct {
     /** Original node. */
-    zvalue node;
+    zvalue orig;
 
     /** Type of the node. */
     zevalType type;
@@ -68,6 +69,34 @@ static ExecNodeInfo *getInfo(zvalue value) {
     return (ExecNodeInfo *) datPayload(value);
 }
 
+/**
+ * Converts a single per-spec executable tree node into an `ExecNode`,
+ * storing the converted result back through the given pointer. As a
+ * convenience, if `*orig` is `NULL`, this function does nothing.
+ */
+static void convertNode(zvalue *orig) {
+    if (*orig != NULL) {
+        *orig = cm_new(ExecNode, *orig);
+    }
+}
+
+/**
+ * Converts each of the elements of the given list into an `ExecNode`,
+ * storing the converted list of all the conversions back through the given
+ * pointer.
+ */
+static void convertList(zvalue *orig) {
+    zarray arr = zarrayFromList(*orig);
+    zvalue result[arr.size];
+
+    for (zint i = 0; i < arr.size; i++) {
+        result[i] = arr.elems[i];
+        convertNode(&result[i]);
+    }
+
+    *orig = listFromZarray((zarray) {arr.size, result});
+}
+
 
 //
 // Module Definitions
@@ -80,7 +109,145 @@ static ExecNodeInfo *getInfo(zvalue value) {
 // Class Definition
 //
 
-// Documented in header.
+/**
+ * Constructs an instance from the given (per spec) executable tree node.
+ */
+CMETH_IMPL_1(ExecNode, new, orig) {
+    zevalType type = recordEvalType(orig);
+    zvalue result = datAllocValue(CLS_ExecNode, sizeof(ExecNodeInfo));
+    ExecNodeInfo *info = getInfo(result);
+
+    info->type = type;
+    info->orig = orig;
+
+    switch (type) {
+        case EVAL_apply:
+        case EVAL_call: {
+            if (!recGet3(orig,
+                    SYM(target), &info->target,
+                    SYM(name),   &info->name,
+                    SYM(values), &info->values)) {
+                die("Invalid `apply` or `call` node.");
+            }
+
+            convertNode(&info->target);
+            convertNode(&info->name);
+            if (type == EVAL_call) {
+                convertList(&info->values);
+            }
+
+            break;
+        }
+
+        case EVAL_closure: {
+            if (!recGet3(orig,
+                    SYM(formals),    &info->formals,
+                    SYM(statements), &info->statements,
+                    SYM(yield),      &info->yield)) {
+                die("Invalid `closure` node.");
+            }
+
+            // These are both optional.
+            recGet2(orig,
+                SYM(name),     &info->name,
+                SYM(yieldDef), &info->yieldDef);
+
+            convertList(&info->statements);
+            convertNode(&info->yield);
+            break;
+        }
+
+        case EVAL_fetch: {
+            if (!recGet1(orig, SYM(target), &info->target)) {
+                die("Invalid `fetch` node.");
+            }
+
+            convertNode(&info->target);
+            break;
+        }
+
+        case EVAL_importModule:
+        case EVAL_importModuleSelection:
+        case EVAL_importResource: {
+            info->statements = makeDynamicImport(orig);
+            convertNode(&info->statements);
+            break;
+        }
+
+        case EVAL_literal:
+        case EVAL_maybe:
+        case EVAL_noYield: {
+            if (!recGet1(orig, SYM(value), &info->value)) {
+                die("Invalid `literal`, `maybe`, or `noYield` node.");
+            }
+
+            if (type != EVAL_literal) {
+                convertNode(&info->value);
+            }
+
+            break;
+        }
+
+        case EVAL_store: {
+            if (!recGet2(orig,
+                    SYM(target), &info->target,
+                    SYM(value),  &info->value)) {
+                die("Invalid `store` node.");
+            }
+
+            convertNode(&info->target);
+            convertNode(&info->value);
+            break;
+        }
+
+        case EVAL_varDef: {
+            zvalue box;
+            if (!recGet3(orig,
+                    SYM(box),   &box,
+                    SYM(name),  &info->name,
+                    SYM(value), &info->value)) {
+                die("Invalid `varDef` node.");
+            }
+
+            info->box = symbolEvalType(box);
+            switch (info->box) {
+                case EVAL_cell:
+                case EVAL_lazy:
+                case EVAL_promise:
+                case EVAL_result: {
+                    // These are all valid.
+                    break;
+                }
+                default: {
+                    die("Invalid `box` spec: %s", cm_debugString(box));
+                }
+            }
+
+            convertNode(&info->value);
+            break;
+        }
+
+        case EVAL_varRef: {
+            if (!recGet1(orig, SYM(name), &info->name)) {
+                die("Invalid `varRef` node.");
+            }
+            break;
+        }
+
+        case EVAL_void: {
+            // Nothing to do.
+            break;
+        }
+
+        default: {
+            die("Invalid node: %s", cm_debugString(orig));
+        }
+    }
+
+    return result;
+}
+
+// Documented in spec.
 METH_IMPL_0(ExecNode, debugSymbol) {
     return getInfo(ths)->name;
 }
@@ -89,10 +256,15 @@ METH_IMPL_0(ExecNode, debugSymbol) {
 METH_IMPL_0(ExecNode, gcMark) {
     ExecNodeInfo *info = getInfo(ths);
 
-    // `statements` might not be covered by `node` (when it was generated via
-    // translation of an `import*`), but everything else is.
-    datMark(info->node);
+    datMark(info->orig);
+    datMark(info->formals);
+    datMark(info->name);
     datMark(info->statements);
+    datMark(info->target);
+    datMark(info->value);
+    datMark(info->values);
+    datMark(info->yield);
+    datMark(info->yieldDef);
 
     return NULL;
 }
@@ -102,7 +274,8 @@ MOD_INIT(ExecNode) {
     MOD_USE(cls);
 
     CLS_ExecNode = makeCoreClass(symbolFromUtf8(-1, "ExecNode"), CLS_Core,
-        NULL,
+        METH_TABLE(
+            CMETH_BIND(ExecNode, new)),
         METH_TABLE(
             METH_BIND(ExecNode, debugSymbol),
             METH_BIND(ExecNode, gcMark)));
