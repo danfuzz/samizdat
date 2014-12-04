4e429d206d9e7d7ac1285cb49668fae2031398d2
Dan Bornstein <danfuzz@milk.com>
2014-12-04 11:26:25 -0800

    Remove most of the global `if*` functions.

--- samex-naif/lib/conditional.c
+++ samex-naif/lib/conditional.c
@@ -13,26 +13,6 @@
 //
 
 // Documented in spec.
-FUN_IMPL_DECL(ifIs) {
-    if (FUN_CALL(args.elems[0]) != NULL) {
-        return FUN_CALL(args.elems[1]);
-    } else if (args.size == 3) {
-        return FUN_CALL(args.elems[2]);
-    } else {
-        return NULL;
-    }
-}
-
-// Documented in spec.
-FUN_IMPL_DECL(ifNot) {
-    if (FUN_CALL(args.elems[0]) == NULL) {
-        return FUN_CALL(args.elems[1]);
-    } else {
-        return NULL;
-    }
-}
-
-// Documented in spec.
 FUN_IMPL_DECL(ifValue) {
     zvalue result = FUN_CALL(args.elems[0]);
 
@@ -43,51 +23,6 @@ FUN_IMPL_DECL(ifValue) {
     } else {
         return NULL;
     }
-}
-
-// Documented in spec.
-FUN_IMPL_DECL(ifValueAnd) {
-    zvalue results[args.size];
-
-    for (zint i = 0; i < args.size; i++) {
-        results[i] = methCall(args.elems[i], SYM(call), (zarray) {i, results});
-
-        if (results[i] == NULL) {
-            return NULL;
-        }
-    }
-
-    return results[args.size - 1];
-}
-
-// Documented in spec.
-FUN_IMPL_DECL(ifValueAndElse) {
-    zint funcCount = args.size - 2;
-    zvalue thenFunc = args.elems[funcCount];
-    zvalue elseFunc = args.elems[funcCount + 1];
-    zvalue results[funcCount];
-
-    for (zint i = 0; i < funcCount; i++) {
-        results[i] = methCall(args.elems[i], SYM(call), (zarray) {i, results});
-
-        if (results[i] == NULL) {
-            return methCall(elseFunc, SYM(call), EMPTY_ZARRAY);
-        }
-    }
-
-    return methCall(thenFunc, SYM(call), (zarray) {funcCount, results});
-}
-
-// Documented in spec.
-FUN_IMPL_DECL(ifValueOr) {
-    for (zint i = 0; i < args.size; i++) {
-        zvalue result = FUN_CALL(args.elems[i]);
-        if (result != NULL) {
-            return result;
-        }
-    }
-
-    return NULL;
 }
 
 // Documented in spec.
